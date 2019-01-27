8c9075355f2009cb7b81a720557102a07fd13b1a
chujacky <jacky9407@gmail.com>
2019-01-26 17:11:07 -0800

    Create simple nav bar

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {    
-      score: 0,
-    }
+    this.state = {
+      predict: true,
+      ranking: false,
+      games: [],
+    };
   }
 
   render() {
     return (
-      <div>
-        Hi
+      <div id="nav">
+        <span id="title">NBA prophet</span>
+        <span>Ranking</span>
+        <span>Predict</span>
       </div>
     );
   }
