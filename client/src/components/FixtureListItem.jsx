74383f6d62b24b11877e6862e82fa2b4d871a6e7
chujacky <jacky9407@gmail.com>
2019-01-27 15:41:38 -0800

    Update state when user change inputs

--- client/src/components/FixtureListItem.jsx
+++ client/src/components/FixtureListItem.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const FixtureListItem = ({ fixture }) => {
+const FixtureListItem = ({ fixture, predict }) => {
   return (
     <div className="fixture">
       <span className="teams">{fixture.hTeam.triCode}</span>
-      <input type="number" />
+      <input type="number" name="hScore" className={fixture.gameId} onChange={e => predict(e)} />
       <span>:</span>
-      <input type="number" />
+      <input type="number" name="vScore" className={fixture.gameId} onChange={e => predict(e)} />
       <span className="teams">{fixture.vTeam.triCode}</span>
     </div>
   );
 };
 
 export default FixtureListItem;
+
+FixtureListItem.propTypes = {
+  fixture: PropTypes.object,
+  predict: PropTypes.func,
+};
