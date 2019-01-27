d94cfb95bd5274488bc520b47583903d9bfca955
chujacky <jacky9407@gmail.com>
2019-01-26 16:23:59 -0800

    Setup development environment

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {    
+      score: 0,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Hi
+      </div>
+    );
+  }
+}
+
+export default App;
